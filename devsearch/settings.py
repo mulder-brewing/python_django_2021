9f60e54b373505caeace2cbe12405c48bfc73852
unknown <44846781+mulder-brewing@users.noreply.github.com>
2022-01-05 18:35:07 -0500

    DB, CRUD, static files & theme

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dkf)s*sbms4)i+sgfbln((w*7v(_w1h-e+f5^9-5ko+$qou2he
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,6 +115,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- projects/views.py
+++ projects/views.py
@@ -1,36 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from projects.models import Project
+from .forms import ProjectForm
 
 
 def projects(request):
     context = {
-        'msg': 'Hello, you are on the projects page',
-        'num': 9,
-        'projects': projectsList
+        'projects': Project.objects.all()
     }
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    project_obj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            project_obj = i
-    return render(request, 'projects/single-project.html', {'project': project_obj})
+    project_obj = Project.objects.get(id=pk)
+    context = {
+        'project': project_obj
+    }
+    return render(request, 'projects/single-project.html', context)
+
+
+def create_project(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'projects/project_form.html', context)
+
+
+def update_project(request, pk):
+    project_obj = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project_obj)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'projects/project_form.html', context)
+
+
+def delete_project(request, pk):
+    project_obj = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        project_obj.delete()
+        return redirect('projects')
+
+    context = {
+        'object': project_obj
+    }
+    return render(request, 'projects/delete_template.html', context)
